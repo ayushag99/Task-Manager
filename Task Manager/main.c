1ae74fc3ba476fd7ebe708e2d58ae692ead529cf
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2018-04-27 14:47:25 +0530

    Initial Commit

---
+++ Task Manager/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Task Manager
+//
+//  Created by Ayush Agrawal on 27/04/18.
+//  Copyright © 2018 Ayush Agrawal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
