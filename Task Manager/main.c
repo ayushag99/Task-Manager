dd6ccfe7bfb6557affc6f8485601b10522256f87
Ayush Agrawal <ayush.ashuvaish@gmail.com>
2018-04-27 15:36:54 +0530

    Basic structure made

--- Task Manager/main.c
+++ Task Manager/main.c
@@ -8,18 +8,53 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main() {
+//    STRUCTURE THAT WILL TAKE DATA OF A TASK
+
+struct todo{
+    char *task;
+    char *date;
+//    bool priority;
+};
+
+//  Dynamic memory allocation to entity strings of structure
+
+char* dynamic(char*ch,struct todo *a){
+    
+    char *p;
+    unsigned long int len;
+    len=strlen(ch);
+    p=(char*)malloc(len+1);
+    strcpy(p, ch);
+    return p;
+}
+
+//  ADD TASKS
+
+void addTasks{
     
     
     
     
+};
+
+int main()
+{
+    struct todo *st = NULL, *ps;
     
     
     
     
+    //MENU
+    printf("MENU:  \n");
+    printf("ADD TAKS");
+    printf("\t 1: Add a Task \n");
+    printf("\t 2: Add Multiple Tasks of same date \n");
+    printf("Mark Completed Tasks \n");
+    printf("\t 3: Tasks Done");
     
-    
+
     
     return 0;
 }
